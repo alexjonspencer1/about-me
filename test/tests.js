a6441319d6b05ed6b6161813660acfd97da23f91
Alex Spencer <alexander.j.spencer@gmail.com>
2019-07-16 10:52:47 -0700

    Read through code, made changes to improve readability, added comments

--- test/tests.js
+++ test/tests.js
@@ -2,40 +2,44 @@ import isYes from '../src/is-yes.js';
 
 const test = QUnit.test;
 
+//Test 1
 test('yes is correct answer', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-   
+
     const response = 'yes';
     const expected = true;
 
     //Act 
     // Call the function you're testing and set the result to a const
-   
+
     const result = isYes(response);
 
     //Assert
-    
+
     assert.equal(result, expected);
+
 });
 
+//Test 2
 test('YES is correct answer', function(assert) {
     //Arrange
     // Set up your parameters and expectations
-   
+
     const response = 'YES';
     const expected = true;
 
     //Act 
     // Call the function you're testing and set the result to a const
-   
+
     const result = isYes(response);
 
     //Assert
-    
+
     assert.equal(result, expected);
 });
 
+//Test 3
 test('no is not the correct answer', function(assert) {
     //Arrange
     // Set up your parameters and expectations
@@ -53,6 +57,7 @@ test('no is not the correct answer', function(assert) {
     assert.equal(result, expected);
 });
 
+//Test 4
 test('Y is a correct answer', function(assert) {
     //Arrange
     // Set up your parameters and expectations
@@ -70,6 +75,7 @@ test('Y is a correct answer', function(assert) {
     assert.equal(result, expected);
 });
 
+//Test 5
 test('n is an incorrect answer', function(assert) {
     //Arrange
     // Set up your parameters and expectations
@@ -87,6 +93,7 @@ test('n is an incorrect answer', function(assert) {
     assert.equal(result, expected);
 });
 
+//Test 6
 test('no input is an incorrect answer', function(assert) {
     //Arrange
     // Set up your parameters and expectations
