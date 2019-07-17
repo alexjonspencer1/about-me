b3dd449ff4766afff9dcc0bb8aa9c4d80911cc6b
Alex Spencer <alexander.j.spencer@gmail.com>
2019-07-17 08:53:03 -0700

    Updated JS code and number game now works

--- src/number-guess.js
+++ src/number-guess.js
@@ -14,34 +14,40 @@ const quizResult = document.getElementById('quiz-result');
 
 let correctNumber = 15;
 let numberRemaining = 4;
-let increment = 1;
-let numberDecrease = numberRemaining -= increment;
 
 // What events do I need to listen to? 
 
 button.addEventListener('click', () => {
+    
+    numberRemaining -= 1;
     
     const userGuessValue = userGuess.value;
     
     let userResponseCorrect = ('You have guessed the correct number!!');
     let userResponseLow = ('Your guess was too low! You have ' + numberRemaining + ' guesses remaining!');
     let userResponseHigh = ('Your guess was too high! You have ' + numberRemaining + ' guesses remaining!');
+    let userResponseLose = ('Woops, you have run out of guesses!! Thanks for playing and feel free to try again!');
+   
+    if(numberRemaining === 0) {
+        button.disabled = true;
+        return quizResult.textContent = userResponseLose;
+    }
+    
+    // User guess is correct
+    if(comparedNumbers(userGuessValue, correctNumber) === undefined) {
+        button.disabled = true;
+        return quizResult.textContent = userResponseCorrect;
+    }
     
     // User guess is too low
-    
+
     if(comparedNumbers(userGuessValue, correctNumber) === -1) {
-        numberDecrease;
         return quizResult.textContent = userResponseLow;
     }
     
     // User guess is too high
     if(comparedNumbers(userGuessValue, correctNumber) === 1) {
-        numberDecrease;
         return quizResult.textContent = userResponseHigh;
     }
     
-    // User guess is correct
-    if(comparedNumbers(userGuessValue, correctNumber) === undefined) {
-        return quizResult.textContent = userResponseCorrect;
-    }
 });
