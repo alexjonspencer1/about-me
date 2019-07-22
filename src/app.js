7e326d9c4dff8ea20492bca12fb2289968adcf62
Alex Spencer <alexander.j.spencer@gmail.com>
2019-07-22 08:35:56 -0700

    Final touches to my about-me page

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 import isYes from './is-yes.js';
 
-//Reference needed DOM elements
 const clickButton = document.getElementById('quiz-start');
 const quizResult = document.getElementById('quiz-result');
 
-//Event Handlers
 clickButton.onclick = function() {
     const firstNameParticipant = prompt ('Please enter your first name!');
     if(firstNameParticipant === null) {
@@ -23,19 +21,16 @@ clickButton.onclick = function() {
 
     alert ('Excellent news, ' + firstNameParticipant + '. Let\'s play the game!');
 
-    //Quiz question 1
     const questionOne = prompt ('My family dog is a 65 pound goldendoodle named Timber (Y/N)');
     if(questionOne === '') {
         return alert('You have to put in something!! Responses are important, so restart the quiz please!');
     } 
 
-    //Quiz question 2
     const questionTwo = prompt ('My wife and I live in Vancouver, WA (Y/N)');
     if(questionTwo === '') {
         return alert('You have to put in something!! Responses are important, so restart the quiz please!');
     }
 
-    //Quiz question 3
     const questionThree = prompt ('My favorite city in the world is Edinburgh, Scotland (Y/N)');
     if(questionThree === '') {
         return alert('You have to put in something!! Responses are important, so restart the quiz please!');
@@ -43,7 +38,6 @@ clickButton.onclick = function() {
 
     alert ('You have completed the quiz, YAY and nice WORK!! Your results will be posted at the bottom of the webpage. I sure hope you did well...');
 
-    //Checking sumbissions agaist the is-yes function
     const responseOne = isYes(questionOne);
     const responseTwo = isYes(questionTwo);
     const responseThree = isYes(questionThree);
@@ -64,7 +58,6 @@ clickButton.onclick = function() {
 
     const numberPercent = Math.floor(numberCorrect / 3 * 100);
 
-    //Result text to user
     let userResponse = ('Hey' + ' ' + firstNameParticipant + ' ' + lastNameParticipant + '. Thanks for taking my quiz, let\'s see how you did! It looks like you got ' + numberCorrect + '/3, or ' + numberPercent + '% correct.');
 
     quizResult.textContent = userResponse;
